8172235dc4d68b17df4318fb8492b65a07d52554
Mateus Guedes <teuguedes@outlook.com>
2020-09-04 11:07:06 -0300

    create database connection

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+// src/routes/index.ts
+import { Router } from 'express';
+const routes = Router();
+
+
+import appointmentsRouter from './appointments.routes'
+
+routes.use('/appointments', appointmentsRouter)
+
+export default routes;
